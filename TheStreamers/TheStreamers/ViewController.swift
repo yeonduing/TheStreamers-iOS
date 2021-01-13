cc80abef0d70f45db6ee77046ece31e11c7f556d
yeonduing <yeonduing@gmail.com>
2021-01-13 12:22:44 +0900

    style: 프로젝트 생성 및 swiftlint 적용

---
+++ TheStreamers/TheStreamers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TheStreamers
+//
+//  Created by 류연수 on 2021/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
